c4da31dd7115d05ba8aef46e7ddf35d229caeddd
askend3d <ramazan01012005@gmail.com>
2024-02-09 20:09:25 +0300

    update-quiz

--- emoji-maker/script.js
+++ emoji-maker/script.js
@@ -1,17 +1,101 @@
 
 
-
 const selectSkinCard = document.getElementById("select-skin-card");
+const selectEyesCard = document.getElementById("select-eyes-card");
+const selectMouthCard = document.getElementById("select-mouth-card")
+
 
 selectSkinCard.addEventListener("click", (event) => {
+    if (event.target.tagName === "I") {
+        selectSkinCard.style.display = "none";
+        selectEyesCard.style.display = "block";
+    }   
     const skinChange = document.getElementById("skin")
     if (event.target.id === "yellow-skin") {
         skinChange.setAttribute("src", "./assets/skin/yellow.png")
+        selectSkinCard.style.display = "none";
+        selectEyesCard.style.display = "block";
+        
     }
     if (event.target.id === "green-skin") {
         skinChange.setAttribute("src", "./assets/skin/green.png")
+        selectSkinCard.style.display = "none";
+        selectEyesCard.style.display = "block";
     }
     if (event.target.id === "red-skin") {
         skinChange.setAttribute("src", "./assets/skin/red.png")
+        selectSkinCard.style.display = "none";
+        selectEyesCard.style.display = "block";
+    }
+})
+
+selectEyesCard.addEventListener("click", (event) => {
+
+    if (event.target.id === "show-mouth-card") {
+        selectMouthCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+
+    if (event.target.id === "show-skin-card") {
+        selectSkinCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+
+    const eyesChange = document.getElementById("eyes");
+
+    if (event.target.id === "eye-normal") {
+        eyesChange.setAttribute("src", "./assets/eyes/normal.png")
+        selectMouthCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+    if (event.target.id === "eye-closed") {
+        eyesChange.setAttribute("src", "./assets/eyes/closed.png")
+        selectMouthCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+    if (event.target.id === "eye-long") {
+        eyesChange.setAttribute("src", "./assets/eyes/long.png")
+        selectMouthCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+    if (event.target.id === "eye-laughing") {
+        eyesChange.setAttribute("src", "./assets/eyes/laughing.png")
+        selectMouthCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+    if (event.target.id === "eye-rolling") {
+        eyesChange.setAttribute("src", "./assets/eyes/rolling.png")
+        selectMouthCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+    if (event.target.id === "eye-winking") {
+        eyesChange.setAttribute("src", "./assets/eyes/winking.png")
+        selectMouthCard.style.display = "block";
+        selectEyesCard.style.display = "none";
+    }
+})
+
+selectMouthCard.addEventListener("click", (event) => {
+    if (event.target.tagName === "I") {
+        selectEyesCard.style.display = "block";
+        selectMouthCard.style.display = "none";
+    }
+
+    const mouthChange = document.getElementById("mouth");
+    console.log(event.target.id)
+    if (event.target.id === "mouth-open") {
+        mouthChange.setAttribute("src", "./assets/mouth/open.png")
+    }
+    if (event.target.id === "mouth-smiling") {
+        mouthChange.setAttribute("src", "./assets/mouth/smiling.png")
+    }
+    if (event.target.id === "mouth-straight") {
+        mouthChange.setAttribute("src", "./assets/mouth/straight.png")
+    }
+    if (event.target.id === "mouth-sad") {
+        mouthChange.setAttribute("src", "./assets/mouth/sad.png")
+    }
+    if (event.target.id === "mouth-teeth") {
+        mouthChange.setAttribute("src", "./assets/mouth/teeth.png")
     }
 })
--- quiz-app/quiz.js
+++ quiz-app/quiz.js
@@ -1,1 +1,78 @@
-const questionsAPI = 'https://5d76bf96515d1a0014085cf9.mockapi.io/quiz';
+async function returnJson() {
+  const questionsAPI = await fetch(
+    "https://5d76bf96515d1a0014085cf9.mockapi.io/quiz"
+  ).then((response) => response.json());
+
+  return questionsAPI;
+}
+
+let arrAnswer = [];
+
+async function fetchDataAndProcess() {
+  let data = await returnJson();
+  arrAnswer = data.map((item) => item.answer);
+}
+
+const divs = document.querySelectorAll("form div")
+let result = 0;
+
+divs.forEach((div, index) => {
+  div.addEventListener("click", (e) => {
+    if (e.target.tagName !== 'INPUT') {
+      return;
+    }
+    const counterSpan = document.querySelector("#aside-counter h3 span");
+
+    const inputName = div.querySelector('input').name;
+    const inputValue = div.querySelector('input').value;
+
+    
+    if (inputName === "q1") {
+      if (inputValue == arrAnswer[0]) {
+        div.classList.add("correct")
+        result += 1
+        
+      } else {
+        div.classList.add("incorrect");
+        const correctOption = Array.from(div.children).find(
+          (child) => child.querySelector("input").value == correctAnswer
+        );
+        correctOption.classList.add("correct");
+      }
+    }
+    if (inputName === "q2") {
+      if (inputValue == arrAnswer[1]) {
+        div.classList.add("correct")
+        result += 1;
+      } else {
+        div.classList.add("incorrect")
+      }
+    }
+    if (inputName === "q3") {
+      if (inputValue == arrAnswer[2]) {
+        div.classList.add("correct")
+        result += 1;
+      } else {
+        div.classList.add("incorrect")
+      }
+    }
+    if (inputName === "q4") {
+      if (inputValue == arrAnswer[3]) {
+        div.classList.add("correct")
+        result += 1;
+      } else {
+        div.classList.add("incorrect")
+      }
+    }
+    if (inputName === "q5") {
+      if (inputValue == arrAnswer[4]) {
+        div.classList.add("correct")
+        result += 1;
+      } else {
+        div.classList.add("incorrect")
+      }
+    }
+    counterSpan.innerHTML = `${result}/5`
+  });
+});
+fetchDataAndProcess()
